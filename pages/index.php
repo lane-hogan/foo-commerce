4c08fae0e1edfbe8e675dab1ffad1ade1fd8bde5
Lane Hogan <lane.hogan1@gmail.com>
2021-12-13 16:52:20 -0500

    Fix formatting

--- pages/index.php
+++ pages/index.php
@@ -1,3 +1,10 @@
+<?php
+require_once('../../settings.php');
+require_once('../../lib/db_util.php');
+
+$result = DBHelper::query('SELECT * FROM products');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,41 +17,28 @@
 </head>
 
 <body>
-<?php
-require_once('../../settings.php');
-require_once('../../lib/db_util.php');
-
-$result = DBHelper::query('SELECT * FROM products');
-// print_r($result->fetchAll());
-
-while($product=$result -> fetch()){
-?>
-
-<div class="d-flex justify-content-center ">
-    <div class="card p-2">
-        <div class="flex align-items-center ">
-            <div class="mt-2">
-                <h4 class="text-uppercase">Foo</h4>
-                <div class="advisor_thumb"><a href="product_detail.php?product_ID=<?= $_GET['product_ID']; ?>"><img src="<?=$product["photo"];?>" alt=""></a>
-                <div class="mt-4">
-                    <h1 class="main-heading mt-0"><?php echo $product["name"];?></h1>
-                    <div class="price"> <?php echo "$".$product["price"];?> </div>
+    <?php while ($product = $result->fetch()) : ?>
+        <div class="d-flex justify-content-center ">
+            <div class="card p-2">
+                <div class="flex align-items-center ">
+                    <div class="mt-2">
+                        <h4 class="text-uppercase">Foo</h4>
+                        <div class="advisor_thumb"><a href="product_detail.php?product_ID=<?= $_GET['product_ID']; ?>"><img src="<?= $product["photo"]; ?>" alt=""></a>
+                            <div class="mt-4">
+                                <h1 class="main-heading mt-0"><?php echo $product["name"]; ?></h1>
+                                <div class="price"> <?php echo "$" . $product["price"]; ?> </div>
+                            </div>
+                        </div>
+                        <div class="image"> <img src="<?php echo $product["image"]; ?>" width="100"></div>
+                    </div>
+                    <button class="btn btn-primary" position="fixed" ; bottom="50px" ; right="10px" ;>Add to cart</button>
                 </div>
             </div>
-            <div class="image"> <img src="<?php echo $product["image"];?>" width="100"></div>
-        </div>
-        <button class="btn btn-primary" position="fixed";  bottom="50px"; right="10px";>Add to cart</button>
-    </div>
-</div>
+        <?php endwhile ?>
 
-<?php
-
-}
-?>
-    <!-- Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-PsUw7Xwds7x08Ew3exXhqzbhuEYmA2xnwc8BuD6SEr+UmEHlX8/MCltYEodzWA4u" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
+        <!-- Bootstrap -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-PsUw7Xwds7x08Ew3exXhqzbhuEYmA2xnwc8BuD6SEr+UmEHlX8/MCltYEodzWA4u" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
 </body>
 
 </html>
-
