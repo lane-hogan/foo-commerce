931e70004e1d6a86c8ef6d76cd61211a689b0c94
TheDeathMuffin <samlongTGLM1@gmail.com>
2021-12-15 17:56:10 -0500

    .

--- pages/index.php
+++ pages/index.php
@@ -1,13 +1,11 @@
 <?php
 $title = 'Foo Commerce';
 require_once('../theme/header.php');
-
 require_once('../settings.php');
 require_once('../lib/db_util.php');
 
 session_start();
 
-<<<<<<< HEAD
 $result = DBHelper::query('SELECT * FROM products');
 ?>
 
@@ -54,22 +52,6 @@ $result = DBHelper::query('SELECT * FROM products');
                             <p class="card-text"><?= "$" . $product["price"]; ?></p>
                             <a href="orders.php" class="btn btn-primary">Add to Cart</a>
                         </div>
-=======
-$result = DBHelper::query('SELECT * FROM products'); ?>
-<div class=" container mt-4">
-    <div class="row" style="margin: 0 auto;">
-        <?php while ($product = $result->fetch()) : ?>
-            <div class="col-md-4" style="padding-bottom: 30px;">
-                <div class="card border border-dark" style="width: 20rem;">
-                    <a href="detail.php?product_ID=<?= $product['product_ID']; ?>">
-                        <img class="p-2 card-img-top" src="<?= $product["image"]; ?>" alt="product_image" width="286" height="230" alt="">
-                    </a>
-                    <hr />
-                    <div class="card-body">
-                        <h5 class="card-title"><?= $product['name'] ?></h5>
-                        <p class="card-text"><?= "$" . $product["price"]; ?></p>
-                        <a href="orders.php" class="btn btn-primary">Add to Cart</a>
->>>>>>> 4c795ff8cad9d81813856e4823888e9e86e05efd
                     </div>
                 </div>
             </div>
