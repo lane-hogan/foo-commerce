793668784f729842d59296fa18e28d0bce0caacf
Rozanz <rojankc89@gmail.com>
2021-12-15 19:33:09 -0500

    working on cart page

--- pages/index.php
+++ pages/index.php
@@ -1,13 +1,11 @@
 <?php
 $title = 'Foo Commerce';
 require_once('../theme/header.php');
-
 require_once('../settings.php');
 require_once('../lib/db_util.php');
 
 session_start();
 
-<<<<<<< HEAD
 $result = DBHelper::query('SELECT * FROM products');
 ?>
 
@@ -31,13 +29,14 @@ $result = DBHelper::query('SELECT * FROM products');
             <div class="form-inline">
                 <a href="../auth/sign_in.php" class="mr-sm-2 btn btn-primary">Sign In</a>
                 <a href="../auth/sign_up.php" class="my-2 my-sm-0 btn btn-primary"">Sign Up</a>
-                        </div>
+            </div>
                 <?php else : ?>
                     <li class=" nav-item">
-                    <a href="../auth/sign_out.php" class="btn btn-primary"">Sign Out</a>
+                     <a href="../auth/sign_out.php" class="btn btn-primary"">Sign Out</a>
                     </li>
                 <?php endif; ?>
     </nav>
+
     <?php
     $result = DBHelper::query('SELECT * FROM products'); ?>
     <div class=" container mt-4">
@@ -54,28 +53,29 @@ $result = DBHelper::query('SELECT * FROM products');
                             <p class="card-text"><?= "$" . $product["price"]; ?></p>
                             <a href="orders.php" class="btn btn-primary">Add to Cart</a>
                         </div>
-=======
-$result = DBHelper::query('SELECT * FROM products'); ?>
-<div class=" container mt-4">
-    <div class="row" style="margin: 0 auto;">
-        <?php while ($product = $result->fetch()) : ?>
-            <div class="col-md-4" style="padding-bottom: 30px;">
-                <div class="card border border-dark" style="width: 20rem;">
-                    <a href="detail.php?product_ID=<?= $product['product_ID']; ?>">
-                        <img class="p-2 card-img-top" src="<?= $product["image"]; ?>" alt="product_image" width="286" height="230" alt="">
-                    </a>
-                    <hr />
-                    <div class="card-body">
-                        <h5 class="card-title"><?= $product['name'] ?></h5>
-                        <p class="card-text"><?= "$" . $product["price"]; ?></p>
-                        <a href="orders.php" class="btn btn-primary">Add to Cart</a>
->>>>>>> 4c795ff8cad9d81813856e4823888e9e86e05efd
+
+     $result = DBHelper::query('SELECT * FROM products'); ?>
+     <div class=" container mt-4">
+        <div class="row" style="margin: 0 auto;">
+            <?php while ($product = $result->fetch()) : ?>
+                <div class="col-md-4" style="padding-bottom: 30px;">
+                    <div class="card border border-dark" style="width: 20rem;">
+                        <a href="detail.php?product_ID=<?= $product['product_ID']; ?>">
+                            <img class="p-2 card-img-top" src="<?= $product["image"]; ?>" alt="product_image" width="286" height="230" alt="">
+                        </a>
+                        <hr />
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $product['name'] ?></h5>
+                            <p class="card-text"><?= "$" . $product["price"]; ?></p>
+                            <a href="orders.php" class="btn btn-primary">Add to Cart</a>
+
+                        </div>
                     </div>
                 </div>
-            </div>
-        <?php endwhile; ?>
+            <?php endwhile; ?>
+        </div>
     </div>
-</div>
+
 
 <!-- Bootstrap -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-PsUw7Xwds7x08Ew3exXhqzbhuEYmA2xnwc8BuD6SEr+UmEHlX8/MCltYEodzWA4u" crossorigin="anonymous"></script>
